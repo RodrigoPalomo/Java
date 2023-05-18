44bf99940196d088020867d3c3ea6422823406bc
Rodrigo Palomo <repmlm@hotmail.com>
2023-05-18 09:37:23 -0300

    Haciendo Constructor, Getter y Setter

--- src/poo/Coche.java
+++ src/poo/Coche.java
@@ -4,14 +4,42 @@ public class Coche {
     private int largo;
     private int ancho;
     private int motor;
-    private int peso;
+    private int peso_plataforma;
+    String color;
+    int peso_total;
+    boolean asientos_cuero, climatizador;
 
+    // CONSTRUCTOR
     public Coche(){
         ruedas = 4;
         largo = 2000;
         ancho = 300;
         motor = 1600;
-        peso = 500;
+        peso_plataforma = 500;
+    }
+    // CONSTRUCTOR
 
+    // GETTER
+    public String dime_largo(){
+        // obligatoriamente tiene que devolver algo(return) porque sino
+        // va a marcar error siempre
+
+        return "El largo del coche es: " + largo; // Capto y proporciono valor
+
+        // Acá si podemos entrar a la variable "largo" aunque sea privada (private)
+        // porque sólo es accesible dentro de la misma clase
+    }
+    // GETTER
+
+    // SETTER
+    // El setter tiene la palabra reservada "void", que indica que no
+    // va a devolver ningún valor, al contrario de un GETTER.
+    public void establece_color(){
+        color = "azul"; // Seteo valor
+    }
+    // SETTER
+
+    public String dime_color(){
+        return "El color del coche es: " + color;
     }
 }
--- src/poo/Uso_Coche.java
+++ src/poo/Uso_Coche.java
@@ -2,7 +2,25 @@ package poo;
 
 public class Uso_Coche {
     public static void main(String[] args) {
-        Coche Renault = new Coche(); // Instanciar una clase.
-        System.out.println("Este coche tiene: " + Renault.ruedas + " ruedas");
+        // Coche Renault = new Coche(); // Instanciar una clase.
+//        System.out.println("Este coche tiene: " + Renault.ruedas + " ruedas");
+
+        // System.out.println(Renault.dime_largo());
+
+        // Esto le da sentido al getter porque ahora es más seguro, ya no podemos
+        // modificar la variable de "largo", es decir, ya no se podrá hacer esto:
+        // Renault.largo = 1000;
+
+        Coche miCoche = new Coche();
+
+        miCoche.establece_color();
+
+        System.out.println(miCoche.dime_color());
+
     }
 }
+// Método SETTERS se denominan métodos definidores (Está encargado de definir o establecer el valor de una propiedad)
+
+
+// Método GETTERS se denominan métodos captadores (Está encargado de captar o proporcionar(devolver) el valor de una propiedad.)
+// Sintaxis: public dato_a_devolver nombre_metodo(){codigo + return}
